7fb9fa49eaa64954e5060e8d07619adff874c62b
Gary Huang <gary_1327@yahoo.com>
2022-12-01 12:36:41 -0500

    Add project files.

---
+++ Advent Of Code 2022/day1Part1.cpp
@@ -0,0 +1,34 @@
+//Day 1: Calorie Counting 
+//part 1
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+/*
+#define ll long long
+
+using namespace std; 
+
+int main() {
+	fstream fstr("Day1Input.txt"); 
+
+	int total = 0; 
+	int curr = 0; 
+	string line; 
+	while (getline(fstr, line)) {
+		if (line.empty()) {
+			if (total < curr) {
+				total = curr; 
+			}
+			curr = 0; 
+		}
+		else {
+			curr += stoi(line); 
+		}
+		
+	}
+
+	cout << total << endl; 
+}
+*/
---
+++ Advent Of Code 2022/day1part2.cpp
@@ -0,0 +1,40 @@
+//Day 1: Calorie Counting 
+//part 2 
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <fstream>
+
+#define ll long long
+
+using namespace std;
+
+int main() {
+	fstream fstr("Day1Input.txt");
+
+	vector<int> top3(3, 0); 
+	top3.reserve(4); 
+
+	int curr = 0;
+	string line;
+	while (getline(fstr, line)) {
+		if (line.empty()) {
+			top3.push_back(curr); 
+			sort(top3.rbegin(), top3.rend()); 
+			top3.pop_back(); 
+			curr = 0;
+		}
+		else {
+			curr += stoi(line);
+		}
+
+	}
+
+	int total = 0; 
+	for (auto i : top3) {
+		total += i; 
+	}
+	cout << total << endl;
+}
